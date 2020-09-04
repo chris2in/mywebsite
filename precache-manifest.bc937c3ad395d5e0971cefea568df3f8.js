aa8d33909d8545738594cbc99c0e21989d0adf6a
chris2in <lyh2in@gmail.com>
2020-09-04 01:07:23 -0500

    Updates

---
+++ precache-manifest.bc937c3ad395d5e0971cefea568df3f8.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5c26e34275dcd55fefefea90ea0d66b5",
+    "url": "/mywebsite/index.html"
+  },
+  {
+    "revision": "fd4768930e807b31b7c8",
+    "url": "/mywebsite/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "8e2a7273976c7a35de8c",
+    "url": "/mywebsite/static/js/2.d5968674.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/mywebsite/static/js/2.d5968674.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "fd4768930e807b31b7c8",
+    "url": "/mywebsite/static/js/main.8059d449.chunk.js"
+  },
+  {
+    "revision": "8bfdc81bc68db2f39e17",
+    "url": "/mywebsite/static/js/runtime-main.2e72d8b5.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/mywebsite/static/media/logo.5d5d9eef.svg"
+  }
+]);
