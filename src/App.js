c94aedf79c09208b40c97da7a4950aa4d61a536f
chris2in <lyh2in@gmail.com>
2020-10-07 00:50:36 -0500

    some helpful comment added, you are welcome, chris from future

--- src/App.js
+++ src/App.js
@@ -1,7 +1,8 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
-
+// website is https://chris2in.github.io/mywebsite/
+//  after change, do a 'npm run deploy' to deploy to ^ website 
 function App() {
   return (
     <div className="App">
